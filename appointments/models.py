48466cda4afac366a03ae7e98ff27518156bd5d1
AnneEberhard <anne.eberhard@gmx.net>
2024-06-07 19:06:33 +0200

    added appointments

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from doctors.models import DoctorProfile
+from patients.models import PatientProfile
+from django.utils import timezone
 
-# Create your models here.
+
+class Appointment(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(default=timezone.now)
+    doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE)
+    patient = models.ForeignKey(PatientProfile, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.doctor.user.get_full_name()} - {self.date}"
